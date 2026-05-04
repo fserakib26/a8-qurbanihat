f898c06c2ffe02e4a364fbe80051809d4fbc077e
fserakib26 <fserakib26@gmail.com>
2026-05-04 20:28:21 +0600

    Signup page added but db connection issue faced

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,12 +2,11 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("process.env.MONGO_DB_URL");
+const client = new MongoClient("mongodb+srv://qurbanihaat:4C4osEAyUOQuKt6n@cluster0.13q78mq.mongodb.net/?appName=Cluster0");
 const db = client.db("qurbanihaat");
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db, {
-    
+    database: mongodbAdapter(db, {    
     client
   }),
   emailAndPassword: { 
